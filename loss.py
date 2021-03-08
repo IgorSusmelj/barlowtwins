58acbb3a266947a5c9c7d1f38c25136f41fb793e
IgorSusmelj <igor_7779@hotmail.com>
2021-03-08 22:21:16 +0000

    Add barlow twin loss

---
+++ loss.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+class BarlowTwinsLoss(torch.nn.Module):
+
+    def __init__(self, device, lambda_param=5e-3):
+        super(CorrelationLoss, self).__init__()
+        self.lambda_param = lambda_param
+        self.device = device
+
+    def off_diagonal(self, x: torch.Tensor):
+        return x.masked_select(~torch.eye(x.size(0), dtype=bool))
+
+    def forward(self, z_a: torch.Tensor, z_b: torch.Tensor):
+        # normalize repr. along the batch dimension
+        z_a_norm = (z_a - z_a.mean(0)) / z_a.std(0) # NxD
+        z_b_norm = (z_b - z_b.mean(0)) / z_b.std(0) # NxD
+
+        N = z_a.size(0)
+        D = z_a.size(1)
+
+        # cross-correlation matrix
+        c = torch.mm(z_a_norm.T, z_b_norm) / N # DxD
+        # loss
+        c_diff = (c - torch.eye(D,device=self.device)).pow(2) # DxD
+        # multiply off-diagonal elems of c_diff by lambda
+        self.off_diagonal(c_diff).mul_(self.lambda_param)
+        loss = c_diff.sum()
+
+        return loss
